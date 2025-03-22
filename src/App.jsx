70d935294ff1d82f5b7b098210d8aab3d779d09b
Devvrat Singh Rathod <rathoddevvratsingh777@gmail.com>
2025-03-22 15:00:39 +0530

    setup fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
     return (
         <div>
             <Router>
                 <Routes>
-                    <Route path="/" element={<Index />} />
-                    {/* test comment */}
+                    <Route path="/" element={<Home />} />
                 </Routes>
             </Router>
         </div>
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
-import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss()],
-});
+    plugins: [react(),
+    tailwindcss(),
+    ],
+})
+
+// import { defineConfig } from 'vite'
+// export default defineConfig({
+//     plugins: [
+//     ],
+// })
