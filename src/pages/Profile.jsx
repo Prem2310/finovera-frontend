cb67ad8c39d627f651e5e519e56d2cd9742d5dcf
Jeet Gajjar <94225096+JeetCoD@users.noreply.github.com>
2025-03-23 09:06:43 +0530

    feat:logout implementation

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,25 @@
-import React from 'react' 
+import React from "react";
+import PageHeading from "../components/ui/PageHeading";
+import Button from "../components/Button";
+import { useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
 function Profile() {
+  const navigate = useNavigate();
   return (
     <div>
-      proffff
+      <PageHeading>Profile</PageHeading>
+      <Button
+        type="danger"
+        onClick={() => {
+          localStorage.removeItem("access_token");
+          navigate("/signin");
+          toast.success("Logged out successfully");
+        }}
+      >
+        Logout
+      </Button>
     </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
